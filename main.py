9a33ef44bf6cd9ffd3fb38dee4f5dd113fa334db
Quang M Nguyen <quangteoqs15@gmail.com>
2023-03-10 16:23:14 +0100

    Able to write command from python to ardurino

--- main.py
+++ main.py
@@ -15,30 +15,50 @@ default_app = firebase_admin.initialize_app(cred, {'databaseURL': url})
 ref = db.reference("Data")
 
 windows = ref.child("window")
-windows.set(True)
 door = ref.child("door")
-door.set(True)
 light = ref.child("light")
-light.set(True)
 smokeAlarm = ref.child("smokeAlarm")
-smokeAlarm.set(True)
 #When smoke alarm reach critical, send to firebase as true else false
 soilDry = ref.child("soilDry")
-soilDry.set(True)
 #When soil moisture reach critical, send to firebase as true else false
 
 arduino = serial.Serial('COM5', 9600, timeout=.1)
 
 #Make a console that take input from user
-def console():
-    while True:
-        command = input("Enter command: ")
-
-
 
 
 while True:
+    command = input("Enter command: ")
     data = arduino.readline().decode().strip()
-    if data:
-        print(data)
+    if command == "w":
+        data = windows.get()
+        if data == True:
+            windows.set(False)
+            arduino.write(b'w')
+            print("Window is closed")
+        else:
+            windows.set(True)
+            arduino.write(b'w')
+            print("Window is opened")
+    elif command == "d":
+        data = door.get()
+        if data == True:
+            door.set(False)
+            arduino.write(b'd')
+            print("Door is closed")
+        else:
+            door.set(True)
+            arduino.write(b'd')
+            print("Door is opened")
+    elif command == "l":
+        data = light.get()
+        if data == True:
+            light.set(False)
+            arduino.write(b'l')
+            print("Light is off")
+        else:
+            light.set(True)
+            arduino.write(b'l')
+            print("Light is on")
+
 
